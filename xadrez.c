b05bdd60e81ff03f17113bd8ce166af3019b70f7
alpha341 <141239314+alpha341@users.noreply.github.com>
2025-04-11 12:34:16 +0000

    VERSÃO 2 - MOVIMENTO DO CAVALO

--- xadrez.c
+++ xadrez.c
@@ -2,16 +2,20 @@
 
 // Desafio de Xadrez - MateCheck
 // Nível novato
+// Nível aventureiro
+
 
 int main() {
-    
+    int i = 0;
+    int movimentoCavalo = 1;
+
+
     //Movimento da torre
     for (int i=0; i<5; i++){
         printf("DIREITA\n");
     }
 
     //Movimento da rainha
-    int i = 0;
     while(i<8){
         printf("ESQUERDA\n");
         i++;
@@ -23,6 +27,17 @@ int main() {
         printf("DIAGONAL/CIMA/DIREITA\n");
         i++;
     }while(i<5);
+
+    
+    while (movimentoCavalo>0) {
+        for (i = 0; i<2; i++){
+            printf("BAIXO\n");
+        }
+
+        printf("ESQUERDA\n");
+        movimentoCavalo--;
+    }  
+
 
     return 0;
 }
